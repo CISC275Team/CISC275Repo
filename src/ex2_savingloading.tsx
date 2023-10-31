bfaab4a02433f5e0f99e84fd3350464d641c1f68
Victor Vasquez <victorbrillos1@gmail.com>
2023-10-31 01:37:16 -0400

    got json working on ex2

--- src/ex2_savingloading.tsx
+++ src/ex2_savingloading.tsx
@@ -2,11 +2,18 @@
 import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import { json } from "stream/consumers";
+import { DegreePlan } from "./interfaces/degreeplan";
+import degreeplan_json from "./ex2_dummydata.json";
+
+//load in json data
+const DEGREEPLANS: DegreePlan[] = degreeplan_json.map(
+    (dp): DegreePlan => ({ ...dp })
+);
 
 //this is the initial data if the user is new
-let loaded_data = "3";
+let loaded_data = DEGREEPLANS;
 //this will be the key we use to access previous data
-const SAVE_KEY = "MY-PAGE-DATA";
+const SAVE_KEY = "MY-PAGE-DEGREEPLANS";
 const previousData = localStorage.getItem(SAVE_KEY);
 //load data if previoudData was found into loaded_data which will either default to 3 or to previous data
 if (previousData !== null) {
@@ -14,16 +21,11 @@ if (previousData !== null) {
 }
 
 export function DegreePlanView(): JSX.Element {
-    const [attemptsLeft, setAttemptsLeft] = useState<string>(loaded_data);
-    const [requestedAttempts, setRequestedAttempts] = useState<string>("");
-    function updateRequestedAttempts(
-        event: React.ChangeEvent<HTMLInputElement>
-    ) {
-        setRequestedAttempts(event.target.value);
-    }
-    function saveData() {
-        localStorage.setItem(SAVE_KEY, JSON.stringify(attemptsLeft));
-    }
-
-    return <div></div>;
+    const [degreePlan, setdegreePlan] = useState<DegreePlan[]>(loaded_data);
+    const showData = () => console.log(degreePlan);
+    return (
+        <div>
+            <Button onClick={showData}> Show JSON</Button>
+        </div>
+    );
 }
---
+++ src/interfaces/course.ts
@@ -0,0 +1,7 @@
+export interface Course {
+    classTitle: string;
+    classCode: number;
+    credits: number;
+    degreeRequirements: string[];
+    coursePrereq: string[];
+}
